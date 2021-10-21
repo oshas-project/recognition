918447cbaceaa0d64aa6361ca93146f302a5761c
Finn <finn@thatsfinn.com>
2021-10-21 15:12:04 +1100

    Added testing while loop for detection

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
+import face_recognition
 from picamera import PiCamera
 from time import sleep
 
 def main():
 	camera = PiCamera()
-	camera.capture('/home/pi/oshas/recognition/images/testing.jpg')
+	camera.resolution = (1296, 972)
+	# camera.rotation = 0
+
+	finn_image = face_recognition.load_image_file('/home/pi/finn.jpg')
+	finn_encoding = face_recognition.face_encodings(sample_image)[0]
+
+	while True:
+		camera.capture('/home/pi/oshas/recognition/images/currentsample.jpg')
+		sample_image = face_recognition.load_image_file('/home/pi/oshas/recognition/images/currentsample.jpg')
+		# handle multiple faces here?
+		sample_encoding = face_recognition.face_encodings(sample_image)[0]
+		results = face_recognition(compare_faces([finn_encoding], sample_encoding))
+		print(results)
 
 if __name__ == "__main__":
     main()
