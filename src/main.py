39330a9b8f3c7b0589fe1f08352e9e67e095340c
Finn <finn@thatsfinn.com>
2021-10-21 13:48:05 +1100

    Changed path of capture

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from time import sleep
 
 def main():
 	camera = PiCamera()
-	camera.capture('/home/pi/testing.jpg')
+	camera.capture('/home/pi/oshas/recognition/images/testing.jpg')
 
 if __name__ == "__main__":
     main()
